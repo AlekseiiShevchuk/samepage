f9f735531137d0fb0f63f1b3bd3822d587370773
Olexiy <shevchuka@gmail.com>
2017-06-29 14:15:05 +0300

    route fix

--- resources/views/translation_items/index.blade.php
+++ resources/views/translation_items/index.blade.php
@@ -6,7 +6,7 @@
             Translation Items
         </div>
 
-            <form style="border: 4px solid #a1a1a1;margin-top: 15px;padding: 10px;" action="/admin/excel-import" class="form-horizontal" method="post" enctype="multipart/form-data">
+            <form style="border: 4px solid #a1a1a1;margin-top: 15px;padding: 10px;" action="/excel-import" class="form-horizontal" method="post" enctype="multipart/form-data">
                 {{ csrf_field() }}
                 <input type="file" name="import_file" />
                 <button class="btn btn-primary">Import File</button>
